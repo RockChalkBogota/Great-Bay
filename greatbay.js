38ad3ab7dc64c260b0852d6a923f30dc34995712
Ryan Dahlke <ryandahlke@gmail.com>
2017-09-09 12:16:29 -0500

    added postItem skeleton

--- greatbay.js
+++ greatbay.js
@@ -9,4 +9,22 @@ var inquirer = require('inquirer');
 var connectionString = require('./connectionstrings.js');
 
 //User posts an item.
+function postItem() {
+    console.log("Posting a new item...\n");
+    var query = connection.query(
+    "INSERT INTO products SET ?",
+    {
+        title:
+        type:
+        price:
+    }
+    function(err, res) {
+        console.log(res.affectedRows + " item inserted!\n");
+        // Call updateItem AFTER the INSERT completes.
+        updateItem();
+    }
+  );
+console.log(query.sql);
+}
 
+//Inquire
